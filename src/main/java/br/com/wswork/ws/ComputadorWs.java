5d67c3036fd6f476501f43fc3653a604ecfe5f8e
Jean Lukas Andrade Restelli <jeanlarestelli@gmail.com>
2020-12-23 16:40:02 -0300

    commit quase pronto

--- src/main/java/br/com/wswork/ws/ComputadorWs.java
+++ src/main/java/br/com/wswork/ws/ComputadorWs.java
@@ -3,9 +3,7 @@ package br.com.wswork.ws;
 import br.com.wswork.model.Computador;
 
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Produces;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import java.util.ArrayList;
@@ -23,9 +21,9 @@ public class ComputadorWs {
         List<Computador> listaDeComputador = new ArrayList<Computador>();
 
         Computador computadorLixo = new Computador(100.00, 2011, 7l, "white");
-        Computador computadorMedio = new Computador(400.00, 2015,1l, "black");
-        Computador computadorBom = new Computador(1300.50,2018,5l, "gray");
-        Computador computadorGamer = new Computador(4000.99,2021,4l, "red");
+        Computador computadorMedio = new Computador(400.00, 2015, 1l, "black");
+        Computador computadorBom = new Computador(1300.50, 2018, 5l, "gray");
+        Computador computadorGamer = new Computador(4000.99, 2021, 4l, "red");
 
         listaDeComputador.add(computadorLixo);
         listaDeComputador.add(computadorMedio);
@@ -34,7 +32,52 @@ public class ComputadorWs {
 
         ResponseBuilder builder = Response.status(Response.Status.OK).entity(listaDeComputador);
 
-            return builder.build();
+        return builder.build();
     }
 
-}
+    @DELETE
+    @Path("/")
+    @Consumes("application/json")
+    public Response delete() {
+
+        List<Computador> listaDeComputador = new ArrayList<Computador>();
+
+        Computador computadorLixo = new Computador(100.00, 2011, 7l, "white");
+        Computador computadorMedio = new Computador(400.00, 2015, 1l, "black");
+        Computador computadorBom = new Computador(1300.50, 2018, 5l, "gray");
+        Computador computadorGamer = new Computador(4000.99, 2021, 4l, "red");
+
+        listaDeComputador.add(computadorLixo);
+        listaDeComputador.add(computadorMedio);
+        listaDeComputador.add(computadorBom);
+        listaDeComputador.add(computadorGamer);
+
+        ResponseBuilder builder = Response.status(Response.Status.OK).entity(listaDeComputador);
+
+        return builder.build();
+    }
+
+    @PUT
+    @Path("/")
+    public Response savePC(Computador computador){
+
+        List<Computador> listaDeComputador = new ArrayList<Computador>();
+
+        Computador computadorLixo = new Computador(100.00, 2011, 7l, "white");
+        Computador computadorMedio = new Computador(400.00, 2015, 1l, "black");
+        Computador computadorBom = new Computador(1300.50, 2018, 5l, "gray");
+        Computador computadorGamer = new Computador(4000.99, 2021, 4l, "red");
+
+        listaDeComputador.add(computadorLixo);
+        listaDeComputador.add(computadorMedio);
+        listaDeComputador.add(computadorBom);
+        listaDeComputador.add(computadorGamer);
+
+        ResponseBuilder builder = Response.status(Response.Status.OK).entity(listaDeComputador);
+
+        return builder.build();
+    }
+
+    }
+
+
